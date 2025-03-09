8b41b748b37709b1278b3646f71c4f62b955ae2c
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2025-03-09 15:32:12 +0000

    Add chapter02/java/begin

---
+++ chapter02/java/begin/appointments/src/main/java/dev/minsalus/appointments/AppointmentsApplication.java
@@ -0,0 +1,44 @@
+package dev.minsalus.appointments;
+
+import java.time.Instant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.annotation.Id;
+
+import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.RouterFunctions;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@SpringBootApplication
+public class AppointmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentsApplication.class, args);
+	}
+
+    @Bean
+    RouterFunction<ServerResponse> routes(AppointmentRepository appointmentRepository) {
+        return RouterFunctions.route()
+            .GET("/", _ -> ServerResponse.ok().body("Welcome to the Appointments API!"))
+            .GET("/appointments", _ -> 
+                ServerResponse.ok().body(appointmentRepository.findAll()))
+            .POST("/appointments", request -> {
+                var appointment = appointmentRepository.save(request.body(Appointment.class));
+                var location = request.uriBuilder().path("/{id}").build(appointment.id());
+                return ServerResponse.created(location).body(appointment);
+            })
+            .DELETE("/appointments", _ -> {
+                appointmentRepository.deleteAll();
+                return ServerResponse.noContent().build();
+            })
+            .build();
+    }
+
+}
+
+record Appointment(@Id Long id, Long patientId, Instant appointmentDate) {}
+
+interface AppointmentRepository extends ListCrudRepository<Appointment, Long> {}
---
+++ chapter02/java/begin/appointments/src/test/java/dev/minsalus/appointments/AppointmentsApplicationTests.java
@@ -0,0 +1,83 @@
+package dev.minsalus.appointments;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.Instant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class AppointmentsApplicationTests {
+
+    @Autowired
+    private AppointmentRepository appointmentRepository;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @BeforeEach
+    void setup() {
+        appointmentRepository.deleteAll();
+    }
+
+    @Test
+    void welcomeMessageShouldBeReturned() {
+        webTestClient.get()
+            .uri("/")
+            .exchange()
+            .expectStatus().isOk()
+            .expectBody(String.class)
+            .isEqualTo("Welcome to the Appointments API!");
+    }
+
+    @Test
+    void appointmentsShouldBeReturned() {
+        var appointment = new Appointment(null, 1L, Instant.now());
+        appointmentRepository.save(appointment);
+
+        webTestClient.get()
+            .uri("/appointments")
+            .exchange()
+            .expectStatus().isOk()
+            .expectBodyList(Appointment.class)
+            .hasSize(1)
+            .value(result -> {
+                Appointment actualAppointment = result.getFirst();
+                assertThat(actualAppointment.id()).isNotNull();
+                assertThat(actualAppointment.patientId()).isEqualTo(1L);
+                assertThat(actualAppointment.appointmentDate().toEpochMilli()).isEqualTo(appointment.appointmentDate().toEpochMilli());
+            });
+    }
+
+    @Test
+    void appointmentShouldBeCreated() {
+        var appointment = new Appointment(null, 2L, Instant.now());
+
+        webTestClient.post()
+            .uri("/appointments")
+            .bodyValue(appointment)
+            .exchange()
+            .expectStatus().isCreated()
+            .expectHeader().valueMatches("Location", ".*/appointments/\\d+");
+
+        assertThat(appointmentRepository.findAll()).hasSize(1);
+    }
+
+    @Test
+	void appointmentsShouldBeDeleted() {
+		var appointment = new Appointment(null, 3L, Instant.now());
+		appointmentRepository.save(appointment);
+
+		webTestClient.delete()
+            .uri("/appointments")
+            .exchange()
+            .expectStatus().isNoContent();
+
+		assertThat(appointmentRepository.findAll()).isEmpty();
+  }
+
+}
